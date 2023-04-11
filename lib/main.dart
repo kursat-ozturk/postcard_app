fadf85f6716260e2175160dc88e10b14eb93243e
Kürşat Öztürk <kursatsoftware@gmail.com>
2023-04-12 00:31:11 +0300

    added confetti animation

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,6 @@ import 'package:confetti/confetti.dart';
 
 void main() {
   runApp(const MyApp());
-  /* runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xffd7e0ff), //#D7E0FF
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey.shade800,
-          title: const Text('Mutlu Bayramlar!'),
-        ),
-        body: Center(
-          child: Image.asset('images/mutlu_bayramlar.jpg'),
-        ),
-      ),
-    ),
-  ));*/
 }
 
 class MyApp extends StatefulWidget {
